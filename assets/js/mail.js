335c1e435b8f408dff7a931f6ea1e13e51d91f89
Hedi Machat <32374946+hedi-mac@users.noreply.github.com>
2023-07-15 20:56:01 +0100

    Update mail.js

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -2,10 +2,10 @@ function sendEmail() {
     oForm = document.forms[0];
     Email.send({
         Host : "smtp.elasticemail.com",
-        Username : "hedimaac@gmail.com",
-        Password : "E9538B66C2E50EDD9B532076954E33B9B9DEEB80AF38735B8C479F4BB8607F9E7934A4EB5DEBD4A6EE401BBEB0EC580C",
-        To : 'hedimaac@gmail.com',
-        From : 'hedimaac@gmail.com',
+        Username : "hedi.machat@etudiant-isi.utm.tn",
+        Password : "FE2831FE8D0B4D236915D4FDC44CB3A7C470",
+        To : 'hedi.machat@etudiant-isi.utm.tn',
+        From : 'hedi.machat@etudiant-isi.utm.tn',
         Subject : oForm.elements["subject"].value,
         Body : "<html>From : <strong>"+oForm.elements["name"].value+"</strong><br/>"+oForm.elements["message"].value+"</html>"
     }).then(
